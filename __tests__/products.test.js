2f64cbec8a30b6a29eb3b64fbe556c929cdea411
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2023-04-04 13:01:55 +0300

    test

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -8,6 +8,7 @@ import dotenv from "dotenv"
 import mongoose from "mongoose"
 import server from "../src/server.js"
 import ProductsModel from "../src/api/products/model.js"
+import { response } from "express"
 
 dotenv.config() // This command forces .env vars to be loaded into process.env. This is the way to do it whenever you can't use -r dotenv/config
 
@@ -24,7 +25,9 @@ const client = supertest(server)
   })
 })
  */
-
+const newProduct = {
+  name: "shi gher",
+}
 const validProduct = {
   name: "A valid product",
   description: "balllablalblabl",
@@ -36,9 +39,10 @@ const notValidProduct = {
   price: 100,
 }
 
+let product
 beforeAll(async () => {
   await mongoose.connect(process.env.MONGO_URL_TEST)
-  const product = new ProductsModel({ name: "test", description: "blalblabla", price: 20 })
+  product = new ProductsModel({ name: "test", description: "blalblabla", price: 20 })
   await product.save()
 })
 // beforeAll is a Jest hook ran before all the tests, usually it is used to connect to the db and to do some initial setup (like inserting some mock data in the db)
@@ -61,10 +65,45 @@ describe("Test APIs", () => {
 
   it("Should test that GET /products returns a success status and a body", async () => {
     const response = await client.get("/products").expect(200)
-    console.log(response.body)
+
+    expect(response.body).toBeDefined()
   })
 
   it("Should test that POST /products with a not valid product returns a 400", async () => {
     await client.post("/products").send(notValidProduct).expect(400)
+  })
+
+  it("Should test that GET /products/:id returns a correct product with valid id", async () => {
+    let id = product._id.toString()
+    console.log(id)
+    const response = await client.get(`/products/${id}`)
+    expect(response.body._id).toBe(id)
+  })
+
+  it("Should test that GET /products/:id with a non-existing id returns a 404", async () => {
+    let id = "123456123456123456123456"
+
+    await client.get(`/products/${id}`).expect(404)
+  })
+
+  it("Should test that PUT /products/:id returns a correct product with valid id", async () => {
+    let id = product._id.toString()
+    console.log(id)
+    let response = await client.put(`/products/${id}`).send(newProduct).expect(200)
+    expect(response.body._id).toBe(id)
+    expect(response.body.name).toBe(newProduct.name)
+    expect(typeof response.body.name).toBe("string")
+  })
+
+  it("Should test that DELETE /products/:id returns response 204 valid id", async () => {
+    let id = product._id.toString()
+
+    await client.delete(`/products/${id}`).expect(204)
+  })
+
+  it("Should test that DELETE /products/:id with a non-existing id returns a 404", async () => {
+    let id = "123456123456123456123456"
+
+    await client.delete(`/products/${id}`).expect(404)
   })
 })
--- src/api/products/index.js
+++ src/api/products/index.js
@@ -12,11 +12,50 @@ productsRouter.get("/", async (req, res, next) => {
   }
 })
 
+productsRouter.get("/:id", async (req, res, next) => {
+  try {
+    const product = await ProductsModel.findById(req.params.id)
+    if (product) {
+      res.send(product)
+    } else {
+      res.status(404).send()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
 productsRouter.post("/", async (req, res, next) => {
   try {
     const newProduct = new ProductsModel(req.body)
     const { _id } = await newProduct.save()
     res.status(201).send({ _id })
+  } catch (error) {
+    next(error)
+  }
+})
+
+productsRouter.put("/:id", async (req, res, next) => {
+  try {
+    const updatedProduct = await ProductsModel.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    if (updatedProduct) {
+      res.send(updatedProduct)
+    } else {
+      res.status(404).send()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+productsRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const product = await ProductsModel.findByIdAndDelete(req.params.id)
+    if (product) {
+      res.status(204).send()
+    } else {
+      res.status(404).send()
+    }
   } catch (error) {
     next(error)
   }
